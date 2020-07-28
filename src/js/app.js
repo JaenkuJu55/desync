ac44ee0474ea01ab74a1f6308a7aef6fec3c26fe
SanderRuusmaa <56891593+SanderRuusmaa@users.noreply.github.com>
2020-07-28 18:38:19 +0300

    uued parallaxid, parem disain

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,34 @@
-function parallax(element, distance, speed) {
+function parallaxAlla(element, distance, speed) {
     const item = document.querySelector(element);
     item.style.transform = `translateY(${distance * speed}px)`;
+    
 }
 
+function parallaxYles(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * -speed}px)`;
+}
+
+function parallaxParemale(element, distance, speed, sync) {
+    const item = document.querySelector(element);
+    item.style.transform = `translate(${distance * speed}px, ${distance * sync}px)`;
+}
+
+
+function backToHome() {
+    window.scrollTo(0,0);
+}
+
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+    //Burgeri anim.
+    burger.classList.toggle('toggle');
+});
+
+
+
 window.addEventListener("scroll", function() {
-    parallax(".desync", window.scrollY, 0.6);
-    parallax(".prod", window.scrollY, 0.67);
+    parallaxYles(".film", window.scrollY, 3);
+    parallaxAlla(".prod", window.scrollY, 0.7);
+    parallaxParemale(".service", window.scrollY, 3,0.7);
 })
