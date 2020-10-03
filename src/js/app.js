8997bf1593f26443bbd06303bdb7a31275df16cc
SanderRuusmaa <56891593+SanderRuusmaa@users.noreply.github.com>
2020-10-03 16:24:17 +0300

    animations added to main page

--- src/js/app.js
+++ src/js/app.js
@@ -46,12 +46,73 @@ const submitSlide = () =>{
     })
 }
 
+//const logo = document.querySelectorAll("#leftSVG path");
+
+//for(let i=0; i<logo.length;i++){
+//    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+//}
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".idea", {
-    scrollTrigger: ".idea",
-    opacity: 0.5,
-    duration: 2,
+gsap.to
+
+//svg line anim
+ScrollTrigger.create({
+    trigger: "#leftSVG",
+    toggleActions: "restart pause resume pause",
+    start: "30% 40%",
+    toggleClass: "active",
+    once:true,
 });
+
+ScrollTrigger.create({
+    trigger: "#rightSVG",
+    toggleActions: "restart pause resume pause",
+    start: "30% 40%",
+    toggleClass: "active",
+    once:true
+});
+
+
+//parallax descript
+gsap.to(".description",{
+    scrollTrigger: {
+        trigger: ".description",
+        start: "top",
+        end: "+=200%",
+        pinSpacing: false,
+        scrub: true,
+        pin:true,
+    },
+});
+
+
+//section animations
+let t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#fromshort',
+        start: "0% 50%",
+    }
+});
+
+t1.from("#fromshort", {y:200, opacity:0, duration: 0.5})
+.from("#vid", {x:-200, opacity: 0, duration: 0.9})
+.from("#ad", {x:200, opacity: 0, duration: 0.7})
+.from("#film", {y:-100, opacity: 0, duration: 0.8})
+
+let t2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".ifyou",
+        start: "50% 50%",
+    }
+})
+
+t2.from(".bulb", {opacity: 0, duration: 1})
+.from(".ifyou", {opacity:0, duration: 2})
+.from(".submitBtn", {y: 100, opacity:0, duration: 2})
+
+
+
+
 
 submitSlide();
